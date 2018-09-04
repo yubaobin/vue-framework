728f536ead8bc3c3852a602f5ec7a75117384bbc
余宝彬 <502275531@qq.com>
2018-09-04 11:43:43 +0800

    添加vue模块

---
+++ src/store/mutation-types.js
@@ -0,0 +1,2 @@
+// 页面切换方式
+export const PERMISSION = 'permission'
---
+++ src/store/util.js
@@ -0,0 +1,26 @@
+/**
+ *  action 异步请求
+ */
+
+import store from './index'
+export const doAction = options => {
+  options = Object.assign({
+    api: function () {
+      return new Promise(() => {})
+    },
+    params: {},
+    mutationName: ''
+  }, options)
+  return new Promise(async (resolve, reject) => {
+    let res = await options.api(options.params)
+    if (res.code === 0 || res.code === '0') {
+      let data = res.data
+      options.mutationName && store.commit(options.mutationName, data)
+      resolve(data, res)
+    } else {
+      reject(res)
+      console.log('返回数据出现问题，请在catch中处理异常流程')
+      console.log(res)
+    }
+  })
+}
